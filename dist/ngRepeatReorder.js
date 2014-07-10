b10cbd1b1e3382857dc218ffa62350192b34725f
unicodesnowman <utfsnowman@gmail.com>
2014-07-10 12:54:22 -0400

    pass first element of list, bugfix

--- dist/ngRepeatReorder.js
+++ dist/ngRepeatReorder.js
@@ -312,7 +312,7 @@
                 return $event.preventDefault();
               },
               stopevent: function($event, $element, $index) {
-                var displacedIndex, obj;
+                var displaced, obj;
                 $element.parent().removeClass("active-drag-below");
                 this.resetMargins();
                 this.resetPosition($element);
@@ -324,16 +324,15 @@
                 }
                 if (this.offset !== 0) {
                   collection = $scope.$eval(rhs);
+                  displaced = collection[$index];
                   obj = collection.splice($index, 1);
-                  if (this.offset < 0) {
-                    displacedIndex = $index + this.offset + 1;
-                    collection.splice(displacedIndex, 0, obj[0]);
-                  } else if (this.offset > 0) {
-                    displacedIndex = $index + this.offset - 1;
-                    collection.splice(displacedIndex, 0, obj[0]);
-                  }
                   if (typeof $scope[onUpdateOrder] === "function") {
-                    $scope[onUpdateOrder](obj, displacedIndex);
+                    $scope[onUpdateOrder](obj[0], displaced);
+                  }
+                  if (this.offset < 0) {
+                    collection.splice($index + this.offset + 1, 0, obj[0]);
+                  } else if (this.offset > 0) {
+                    collection.splice($index + this.offset - 1, 0, obj[0]);
                   }
                 }
                 $element.removeClass('dragging');
